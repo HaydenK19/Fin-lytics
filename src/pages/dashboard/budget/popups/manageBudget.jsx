fde4137601d88bb84f143f204b2add956cd5ec6b
Thomas Pearson <pearson.e.thomas@gmail.com>
2025-09-26 21:15:57 -0400

    implemented stock page, and styling

--- src/pages/dashboard/budget/popups/manageBudget.jsx
+++ src/pages/dashboard/budget/popups/manageBudget.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../../../../components/popups/modal.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
-import "./manbud.scss";
+import "./manBud.scss";
 
 const ManageBudgets = ({ onClose }) => {
     const [annualBudget, setAnnualBudget] = useState({
