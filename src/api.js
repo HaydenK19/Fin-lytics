3e92f24126054da5057a4f005d24b55af9934997
HaydenK19 <haydenkomasz@gmail.com>
2025-12-07 22:58:00 -0500

    fix: Use hostname-based detection for frontend API baseURL instead of NODE_ENV

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: window.location.hostname === 'fin-lytics.com' ? 'https://fin-lytics.com' : "http://127.0.0.1:8000",
   withCredentials: true,
 });
 
